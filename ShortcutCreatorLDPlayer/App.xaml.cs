6ed6c3f09aa3435e1320a0902d5bd5c67ad210c3
Shazzaam <fifaprofile21@outlook.com>
2022-08-10 08:46:13 +0200

    Add project files.

---
+++ ShortcutCreatorLDPlayer/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShortcutCreatorLDPlayer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ ShortcutCreatorLDPlayer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace ShortcutCreatorLDPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            string Path, IName, AName;
+            try
+            {
+                Path = LDPlayerPath.Text;
+                IName = InstanceName.Text;
+                AName = InstanceName.Text;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Inputs are not strings.");
+                return;
+            }
+
+        }
+    }
+}
