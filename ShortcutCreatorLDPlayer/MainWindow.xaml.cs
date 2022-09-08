70d31fc3b5885ca47976c9c5c70622e632d4272a
Shazzaam <fifaprofile21@outlook.com>
2022-09-08 15:18:56 +0200

    Update 5 aka V2
    - Increased the height and width of the Window
    - Completely redone code from scratch excluding of the things under click button event.
    - Added Installation Path Finder. Should be able to find install of your LDPlayer
    - Added Installed Apps Finder and a ComboBox with it so you don't need to google play and find package name for your app ;)
    - Default Instance Name is Main, like it is in LDPlayer

--- ShortcutCreatorLDPlayer/MainWindow.xaml.cs
+++ ShortcutCreatorLDPlayer/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using IWshRuntimeLibrary;
+using Microsoft.Win32;
 
 
 namespace ShortcutCreatorLDPlayer
@@ -23,42 +24,45 @@ namespace ShortcutCreatorLDPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string Path, IName, AName, SName, SaveDirectory;
+        string doSet = "\"MY_VAR =%% I\"";
+        Dictionary<string, string> Apps = new Dictionary<string, string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            InstallationPathFinder();
+            FindInstalledApps();
         }
 
-        string Path, IName, AName, SaveDirectory, SName;
-        string doSet = "\"MY_VAR =%% I\"";
+        // Events
 
-        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        private void CreateShortcut_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                Path = LDPlayerPath.Text.ToString();
-                IName = InstanceName.Text.ToString();
-                AName = AppName.Text.ToString();
-                SName = ShortcutName.Text.ToString();
+                Path = InstallationPath.Text;
+                IName = InstanceName.Text;
+                AName = InstalledApps.SelectedItem.ToString();
+                SName = ShortcutName.Text;
                 SaveDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\XuanZhi9\";
             }
-            catch 
+            catch
             {
-                MessageBox.Show("Needs to be a string");
                 return;
             }
-            if (Path.Length <= 0)
+            if (IName.Length <= 0)
             {
-                //Path = "\"C:\\LDPlayer\\LDPlayer9\\\"";
-                Path = @"C:\LDPlayer\LDPlayer9\";
+                IName = "Main";
             }
 
-            string temp = SaveDirectory + SName + ".bat";
-
-            using (StreamWriter sw = new StreamWriter(temp))
+            string tempSave = SaveDirectory + SName + ".bat";
+            using (StreamWriter sw = new StreamWriter(tempSave))
             {
                 sw.Write("@echo off" +
                     "\ncd " + Path +
-                    "\ndnconsole.exe launch --name " + IName +
+                    "\ndnconsole.exe launch --name " + IName + 
                     "\n:waitt" +
                     "\nTimeout 10" +
                     "\n@SET MY_VAR=" +
@@ -70,17 +74,92 @@ namespace ShortcutCreatorLDPlayer
                     "\n)" +
                     "\n:end" +
                     "\nbreak;"
-              );
+                );
             }
 
-            object shDesktop = (object)"Desktop";
+            object Desktop = (object)"Desktop";
             WshShell shell = new WshShell();
-            string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\" + SName + ".lnk";
-            IWshShortcut ShortCut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
-            ShortCut.Description = "Shortcut for " + SName;
-            ShortCut.TargetPath = temp;
-            ShortCut.IconLocation = @"C:\LDPlayer\LDPlayer9\apk_icon.ico";
-            ShortCut.Save();
+            string ShortcutAddress = (string)shell.SpecialFolders.Item(ref Desktop) + @"\" + SName + ".lnk";
+            IWshShortcut Shortcut = (IWshShortcut)shell.CreateShortcut(ShortcutAddress);
+            Shortcut.Description = "Shortcut for " + SName + " installed on LDPlayer";
+            Shortcut.TargetPath = tempSave;
+            Shortcut.IconLocation = @"C:\LDPlayer\LDPlayer9\apk_icon.ico";
+            Shortcut.Save();
+        }
+
+        private void InstalledApps_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedItem;
+            try
+            {
+                selectedItem = InstalledApps.SelectedItem.ToString();
+            }
+            catch 
+            {
+                return;
+            }
+
+            foreach (string key in Apps.Keys)
+            {
+                if (Apps[key] == selectedItem)
+                {
+                    ShortcutName.Text = key;
+                }
+            }
+        }
+
+        // Methods
+        private void InstallationPathFinder()
+        {
+            RegistryKey finder = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+            string location = FindByDisplayName(finder, "LDPlayer");
+            InstallationPath.Text = location.Remove(location.LastIndexOf('\\')) + "\\";
+            Path = InstallationPath.Text;
+        }
+
+        private string FindByDisplayName(RegistryKey AllInstalledSoftware, string Name)
+        {
+            string[] nameList = AllInstalledSoftware.GetSubKeyNames();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                RegistryKey tempInstallation = AllInstalledSoftware.OpenSubKey(nameList[i]);
+                if (tempInstallation.GetValue("DisplayName") == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    try
+                    {
+                        if (string.Equals(tempInstallation.GetValue("DisplayName").ToString(), Name))
+                        {
+                            return tempInstallation.GetValue("DisplayIcon").ToString();
+                        }
+                    }
+                    catch { }
+                }
+            }
+            return "";
+        }
+
+        private void FindInstalledApps()
+        {
+            string AppList = Path + "appNames.text";
+            using (StreamReader sr = new StreamReader(AppList))
+            {
+                string line = sr.ReadLine();
+                string[] splitLine = line.Split('|');
+                splitLine = Array.FindAll(splitLine, i => i != "").ToArray();
+                foreach (string item in splitLine)
+                {
+                    string[] tempSplit = item.Split(':');
+                    Apps.Add(tempSplit[1], tempSplit[0]);
+                }
+                foreach (string key in Apps.Keys)
+                {
+                    InstalledApps.Items.Add(Apps[key]);
+                }
+            }
         }
     }
 }
