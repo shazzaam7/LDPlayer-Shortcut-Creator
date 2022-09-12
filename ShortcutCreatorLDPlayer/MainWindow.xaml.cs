7ef0e6e5f8f250654dcaf7e5e87daa3d18687587
Shazzaam <fifaprofile21@outlook.com>
2022-09-12 18:27:20 +0200

    Update 7
    - Removed the Messagebox that showed where your dnconsole.exe was located
    - Added MessageBox at the start that asks you if you want to run FirstRun cleaup of old AppNames.Text. It will create "firstrun.fr" as a check to know if it asked you this (Don't worry, it's just an empty file)
    - Added FirstRun that deletes old AppNames.Text and  tells user to launch LDPlayer and open apps that they want to create Shortcut off so they appear in the newly created AppNames.Text.
    - Added some documentation
    - Added app.manifest to force the app to be Run as Administrator (Still trying to find a way to run dnconsole.exe without administrator and read the output of it)
    - Added backup solution if Icon is not found on Play Store (Uses default .APK icon of LDPlayer)
    - Added a CheckBox for automatically making Shortcut Run As Administrator (Because dnconsole.exe requires to be Run As Administrator so think of it as moving the step to Run As Administrator from when the .bat script is already open and waiting for UAC to be accepted to before running the script, should bring more consistency to the .bat script)
    - Moved IconGrabber method to try catch so the program doesn't break if it tries to download a icon that doesn't exist
    - Renamed ShortcutAddress to ShortcutLocation
    - Changed CheckIfRunAsAdmin so it returns bool
    - Changed Active Solution Platform from AnyCPU to x64 to reduce the size of the program when building it would create MagickImage dll for x32 that wasn't being used and was just taking space

--- ShortcutCreatorLDPlayer/MainWindow.xaml.cs
+++ ShortcutCreatorLDPlayer/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using IWshRuntimeLibrary;
 using Microsoft.Win32;
 using ImageMagick;
 using System.Diagnostics;
-using System.Reflection;
 using System.Security.Principal;
 
 
@@ -31,28 +30,48 @@ namespace ShortcutCreatorLDPlayer
         public MainWindow()
         {
             InitializeComponent();
-            CheckIfRunAsAdmin();
-            InstallationPathFinder();
-            FindInstances();
-            CheckIfFoldersExist();
-            FindInstalledApps();
-            GC.Collect();
+            //Check to see if the program is running as administrator, if not then it shows a MessageBox and exits
+            if (!CheckIfRunAsAdmin())
+            {
+                MessageBox.Show("Run as administrator!");
+                Environment.Exit(0);
+            }
+            CheckIfFoldersExist(); //Checks if all of the folders necessary for this program to work exist
+            InstallationPathFinder(); //Finds installation folder of LDPlayer
+            if (!System.IO.File.Exists("firstrun.fr"))
+            {
+                MessageBoxResult result = MessageBox.Show("Do you want to run First Run setup to make sure the program works properly?"," ", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    FirstRun();
+                } else
+                {
+                    System.IO.File.Create("firstrun.fr");
+                }
+            }
+            FindInstances(); //Finds all of the instances in LDPlayer (Requires Administrator)
+            FindInstalledApps(); //Finds all of the apps installed in LDPlayer
+            GC.Collect(); //Collects unnecessary garbage
         }
 
         // Events
 
         private void CreateShortcut_Click(object sender, RoutedEventArgs e)
         {
+            //Checks if an Instance has been selected
             if (Instances.SelectedIndex < 0)
             {
                 MessageBox.Show("You have to choose the instance.");
                 return;
             }
+            //Checks if a Installed App has been selected
             if (InstalledApps.SelectedIndex < 0)
             {
                 MessageBox.Show("You have to choose the app you want to create shortcut for.");
                 return;
             }
+
+            //Tries to assign everything to their variable
             try
             {
                 Path = InstallationPath.Text;
@@ -64,15 +83,20 @@ namespace ShortcutCreatorLDPlayer
             {
                 return;
             }
+
+            //If Instance is somehow empty string, it defaults to default value
             if (IName.Length <= 0)
             {
                 IName = "LDPlayer";
             }
-            string tempSave = CustomShortcutDirectory + @"\" + SName + ".bat";
+
+            string tempSave = CustomShortcutDirectory + @"\" + SName + ".bat"; //Where .bat script is stored
             using (StreamWriter sw = new StreamWriter(tempSave))
             {
+                //Checks if the installation folder is in C: or other drive
                 if (Path.StartsWith("C:"))
                 {
+                    //.bat script creation
                     sw.Write("@echo off" +
                         "\ncd " + Path +
                         "\ndnconsole.exe launch --name " + IName +
@@ -91,6 +115,7 @@ namespace ShortcutCreatorLDPlayer
                 }
                 else
                 {
+                    //.bat script creation
                     sw.Write("@echo off" +
                         "\ncd /d " + Path +
                         "\ndnconsole.exe launch --name " + IName +
@@ -108,15 +133,36 @@ namespace ShortcutCreatorLDPlayer
                     );
                 }
             }
-            IconGrabber();
+            try
+            {
+                IconGrabber(); //Method used to grab icons from Play Store
+            }
+            catch
+            {
+
+            }
             object Desktop = (object)"Desktop";
-            WshShell shell = new WshShell();
-            string ShortcutAddress = (string)shell.SpecialFolders.Item(ref Desktop) + @"\" + SName + ".lnk";
-            IWshShortcut Shortcut = (IWshShortcut)shell.CreateShortcut(ShortcutAddress);
-            Shortcut.Description = "Shortcut for " + SName + " installed on LDPlayer";
-            Shortcut.TargetPath = tempSave;
-            Shortcut.IconLocation = IconsDirectory + @"\" + SName + ".ico";
-            Shortcut.Save();
+            WshShell shell = new WshShell(); //New shell
+            string ShortcutLocation = (string)shell.SpecialFolders.Item(ref Desktop) + @"\" + SName + ".lnk"; //Location of the Shortcut (Desktop)
+            IWshShortcut Shortcut = (IWshShortcut)shell.CreateShortcut(ShortcutLocation); //Creating new Shortcut
+            Shortcut.Description = "Shortcut for " + SName + " installed on LDPlayer"; //Description of the Shortcut
+            Shortcut.TargetPath = tempSave; //Where .bat script is saved
+            if (System.IO.File.Exists(IconsDirectory + @"\" + SName + ".ico"))
+            {
+                Shortcut.IconLocation = IconsDirectory + @"\" + SName + ".ico";
+            } else
+            {
+                Shortcut.IconLocation = Path + "apk_icon.ico";
+            }
+            Shortcut.Save(); //Save the Shortcut
+            if (ShortcutAsAdmin.IsChecked == true)
+            {
+                using (FileStream fs = new FileStream(ShortcutLocation, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    fs.Seek(21, SeekOrigin.Begin);
+                    fs.WriteByte(0x22);
+                }
+            }
         }
 
         private void InstalledApps_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -141,35 +187,68 @@ namespace ShortcutCreatorLDPlayer
         }
 
         // Methods
-
-        private void CheckIfRunAsAdmin()
+        private void FirstRun()
         {
-            var identity = WindowsIdentity.GetCurrent();
-            var principal = new WindowsPrincipal(identity);
-            if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+            if (!System.IO.File.Exists("firstrun.fr"))
             {
-                MessageBox.Show("Run as administrator!");
+                System.IO.File.Delete(Path + "appNames.text");
+                MessageBox.Show("First Run only: Current appNames.text has been deleted. Now open LDPlayer and launch all of the applications that you want to create shortcut off");
+                System.IO.File.Create("firstrun.fr");
                 Environment.Exit(0);
             }
         }
 
+        private bool CheckIfRunAsAdmin()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
         private void InstallationPathFinder()
         {
+            //Opens RegistryKey where LDPlayer9 is located
             RegistryKey InstallPathLocation = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\XuanZhi\LDPlayer9");
+            //Grabs the value of InstallDir in LDPlayer9 registry folder
             string location = InstallPathLocation.GetValue("InstallDir").ToString();
+            //Shows the value of InstallDir on InstallationPath TextBox
             InstallationPath.Text = location;
+            //Giving values to variables that are needed in the future
             CustomShortcutDirectory = location + "CustomShortcuts";
             IconsDirectory = CustomShortcutDirectory + @"\Icons";
             Path = InstallationPath.Text;
         }
 
+        private void FindInstances()
+        {
+            //Uses Process to open dnconsole.exe with arguments needed to list all of the instances and then stores them in a array
+            string console = Path;
+            Process findInstance = new Process();
+            findInstance.StartInfo.UseShellExecute = false;
+            findInstance.StartInfo.FileName = Path + "dnconsole.exe";
+            findInstance.StartInfo.RedirectStandardOutput = true;
+            findInstance.StartInfo.CreateNoWindow = true;
+            findInstance.StartInfo.Arguments = "list";
+            findInstance.Start();
+            findInstance.WaitForExit();
+            string output = findInstance.StandardOutput.ReadToEnd();
+            string[] outputSorted = output.Split(Environment.NewLine.ToCharArray()); //Converts string to array (Instances are stored in rows)
+            outputSorted = Array.FindAll(outputSorted, i => i != "").ToArray(); //Removes all of the empty strings in the array
+            foreach (string item in outputSorted)
+            {
+                Instances.Items.Add(item); //Stores all of the instances in Instances ComboBox
+            }
+        }
+
         private void CheckIfFoldersExist()
         {
-            string IconsDirectory = CustomShortcutDirectory + @"\Icons";
+            string IconsDirectory = CustomShortcutDirectory + @"\Icons"; //Icons direcotry where program stores all of the downloaded icons
+            //Checks if CustomShortcutDirecotry exists and creates one if it doesn't already exist
             if (!Directory.Exists(CustomShortcutDirectory))
             {
                 Directory.CreateDirectory(CustomShortcutDirectory);
             }
+            //Checks if IconsDirecotry exists and creates one if it doesn't already exist
             if (!Directory.Exists(IconsDirectory))
             {
                 Directory.CreateDirectory(IconsDirectory);
@@ -178,72 +257,58 @@ namespace ShortcutCreatorLDPlayer
 
         private void FindInstalledApps()
         {
-            string AppList = Path + "appNames.text";
+            string AppList = Path + "appNames.text"; //Directory of the text file that stores all of the apps installed on LDPlayer
+            //Checks if the text file exists.
             if (!System.IO.File.Exists(AppList))
             {
                 MessageBox.Show("You need to run your applications first in LDPlayer to be able to create shortcuts");
                 Environment.Exit(0);
             }
 
+            //StreamReader reads the text file
             using (StreamReader sr = new StreamReader(AppList))
             {
-                string line = sr.ReadLine();
-                string[] splitLine = line.Split('|');
-                splitLine = Array.FindAll(splitLine, i => i != "").ToArray();
+                string line = sr.ReadLine(); //Text File has everything stored in 1 line
+                string[] splitLine = line.Split('|'); //Apps are split with this character
+                splitLine = Array.FindAll(splitLine, i => i != "").ToArray(); //Removing empty strings
+                //Now that all apps are split, we need to split package name and app name from each other
                 foreach (string item in splitLine)
                 {
-                    string[] tempSplit = item.Split(':');
-                    Apps.Add(tempSplit[1], tempSplit[0]);
+                    string[] tempSplit = item.Split(':'); //Splits Package Name and App Name
+                    Apps.Add(tempSplit[1], tempSplit[0]); //Storing all of the App Name and Package Name in a Dictionary
                 }
                 foreach (string key in Apps.Keys)
                 {
-                    InstalledApps.Items.Add(Apps[key]);
+                    InstalledApps.Items.Add(Apps[key]); //Showing all of the Package names in the InstalledApps ComboBox
                 }
             }
         }
 
         private void IconGrabber()
         {
+            //Checks if the icon is already downloaded so it doesn't download it again
             if (!System.IO.File.Exists(IconsDirectory + @"\" + ShortcutName.Text + ".ico"))
             {
-                WebClient wClient = new WebClient();
-                var src = "https://play.google.com/store/apps/details?id=" + InstalledApps.SelectedItem.ToString();
-                string srcHTML = wClient.DownloadString(src);
-                HtmlDocument doc = new HtmlDocument();
-                doc.LoadHtml(srcHTML);
-                var ImageURL = doc.DocumentNode.SelectSingleNode("//img").Attributes["src"].Value; //img[@alt='Icon Image']
+                WebClient wClient = new WebClient(); //WebClient
+                var src = "https://play.google.com/store/apps/details?id=" + InstalledApps.SelectedItem.ToString(); //Link of the selected app in Play Store
+                string srcHTML = wClient.DownloadString(src); //Downloads the HTML of the site
+                HtmlDocument doc = new HtmlDocument(); //Instancing new doc
+                doc.LoadHtml(srcHTML); //Loading the downloaded HTML
+                var ImageURL = doc.DocumentNode.SelectSingleNode("//img").Attributes["src"].Value; //Trying to find img in the HTML
+                //WebClient downloads the icon as a .png
                 using (WebClient client = new WebClient())
                 {
-                    client.DownloadFile(new Uri(ImageURL), IconsDirectory + @"\" + ShortcutName.Text + ".png");
+                    client.DownloadFile(new Uri(ImageURL), IconsDirectory + @"\" + ShortcutName.Text + ".png"); //Downloads the Icon in the correct spot
                 }
+                //MagickImage converts the icon from .png to .ico
                 using (MagickImage convertImage = new MagickImage(IconsDirectory + @"\" + ShortcutName.Text + ".png"))
                 {
                     convertImage.Write(IconsDirectory + @"\" + ShortcutName.Text + ".ico");
                 }
+                //Deletes old .png file
                 System.IO.File.Delete(IconsDirectory + @"\" + ShortcutName.Text + ".png");
             }
         }
 
-        private void FindInstances()
-        {
-
-            string console = Path;
-            Process findInstance = new Process();
-            findInstance.StartInfo.UseShellExecute = false;
-            findInstance.StartInfo.FileName = Path + "dnconsole.exe";
-            findInstance.StartInfo.RedirectStandardOutput = true;
-            findInstance.StartInfo.CreateNoWindow = true;
-            MessageBox.Show(Path + "dnconsole.exe");
-            findInstance.StartInfo.Arguments = "list";
-            findInstance.Start();
-            string output = findInstance.StandardOutput.ReadToEnd();
-            findInstance.WaitForExit();
-            string[] outputSorted = output.Split(Environment.NewLine.ToCharArray());
-            outputSorted = Array.FindAll(outputSorted, i => i!= "").ToArray();
-            foreach (string item in outputSorted)
-            {
-                Instances.Items.Add(item);
-            }
-        }
     }
 }
